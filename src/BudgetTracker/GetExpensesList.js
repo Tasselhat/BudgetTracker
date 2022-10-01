6e90453997881a20c8104bd6989883b885df88d5
Tim <tim.b.schneider@gmail.com>
2022-09-30 19:50:04 -0700

    changes

--- src/BudgetTracker/GetExpensesList.js
+++ src/BudgetTracker/GetExpensesList.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
-import { BudgetSubmitter } from "./BudgetSubmitter";
+import "../css/TrackerRings.css";
 
 const GetExpensesList = (updatedKey) => {
 	const [budget, setBudget] = useState();
@@ -36,55 +36,59 @@ const GetExpensesList = (updatedKey) => {
 	}, [axiosPrivate]);
 
 	return (
-		<article>
-			<h2>Current Income:</h2>
-			<p>${budget?.income}</p>
-			<h2>Saving allocation:</h2>
-			{budget?.saving?.total ? ( //is there a total
-				budget?.saving?.percent ? ( //if there is a total is there a percent
-					<p>
-						${budget.saving.total} or {budget.saving.percent}% (whichever is
-						greater)
-					</p>
+		<div className="expense-list-wrapper">
+			<article className="expense-list">
+				<h2>Current Income:</h2>
+				{budget?.income ? (<p>${budget.income}</p>) : ( <p>No income found</p> )}
+				<br/>
+				<h2>Saving allocation:</h2>
+				{budget?.saving?.total ? ( //is there a total
+					budget?.saving?.percent ? ( //if there is a total is there a percent
+						<p>
+							${budget.saving.total} or {budget.saving.percent}% of income (whichever is
+							greater)
+						</p>
+					) : (
+						//yes total no percent?
+						<p>${budget.saving.total}</p>
+					)
+				) : budget?.saving?.percent ? ( //no total, is there a percent?
+					<p>{budget.saving.percent}% of income</p>
 				) : (
-					//yes total no percent?
-					<p>${budget.saving.total}</p>
-				)
-			) : budget?.saving?.percent ? ( //no total, is there a percent?
-				<p>{budget.saving.percent}% of income</p>
-			) : (
-				//no total, no percent.
-				<p>No saving allocation</p>
-			)}
-			<h2>Expense List</h2>
-			{budget?.expenses?.length ? (
-				<ul>
-					{budget.expenses.map((expenses, i) => (
-						<li key={i}>
-							{expenses?.expenseName} {expenses?.expensePercentage}% of income
-							or ${expenses?.expenseFlatCost}
-						</li>
-					))}
-					<br />
-				</ul>
-			) : (
-				<h2>No Expenses found</h2>
-			)}
-			<h2>Saving/Investments List</h2>
-			{budget?.savings?.length ? (
-				<ul>
-					{budget.savings.map((savings, i) => (
-						<li key={i}>
-							{savings?.savingName} {savings?.savingPercentage}% of income or $
-							{savings?.savingFlatCost}
-						</li>
-					))}
-					<br />
-				</ul>
-			) : (
-				<h2>No Expenses found</h2>
-			)}
-		</article>
+					//no total, no percent.
+					<p>No saving allocation</p>
+					)}
+				<br/>
+				<h2>Expense List</h2>
+				{budget?.expenses?.length ? (
+					<ul>
+						{budget.expenses.map((expenses, i) => (
+							<li key={i}>
+								{expenses?.expenseName} {expenses?.expensePercentage}% of income
+								or ${expenses?.expenseFlatCost}
+							</li>
+						))}
+						<br />
+					</ul>
+				) : (
+					<h2>No Expenses found</h2>
+				)}
+				<h2>Saving/Investments List</h2>
+				{budget?.savings?.length ? (
+					<ul>
+						{budget.savings.map((savings, i) => (
+							<li key={i}>
+								{savings?.savingName} {savings?.savingPercentage}% of income or
+								${savings?.savingFlatCost}
+							</li>
+						))}
+						<br />
+					</ul>
+				) : (
+					<h2>No Expenses found</h2>
+				)}
+			</article>
+		</div>
 	);
 };
 
