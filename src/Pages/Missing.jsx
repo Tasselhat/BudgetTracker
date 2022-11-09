b6d3b5315c2024b08cd5448a994f5a2aff6aefe0
Tim <tim.b.schneider@gmail.com>
2022-11-08 22:19:35 -0800

    missing page updated

--- src/Pages/Missing.jsx
+++ src/Pages/Missing.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Missing() {
-  return <h1>Page Does Not Exist</h1>;
+	return (
+		<div className="center">
+			<h1>Page Does Not Exist</h1>;
+		</div>
+	);
 }
